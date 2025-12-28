01025f4379418064b37be039a28818b3a5c7b940
Aritra Banerjee <banerjeearitra63@gmail.com>
2025-12-28 23:49:00 +0530

    update-4.0

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import '../style/style.css'
+import { Link } from 'react-router-dom'
+import Task1 from '../pages/Task1'
 
 const Header = () => {
-  return (
-    <nav class="navbar">
-        <ul class="nav-links">
-            <li><a href="/task-1">Task 1</a></li>
-            <li><a href="/task-2">Task 2</a></li>
-            <li><a href="/task-3">Task 3</a></li>
-            <li><a href="/task-4">Task 4</a></li>
-            <li><a href="/task-5">Task 5</a></li>
-            <li><a href="/task-6">Task 6</a></li>
-        </ul>
-    </nav>
-  )
+    return (
+        <nav class="navbar">
+            <ul class="nav-links">
+                <li><Link to="/task-1">Task 1</Link></li>
+                <li><Link to="/task-2">Task 2</Link></li>
+                <li><Link to="/task-3">Task 3</Link></li>
+                <li><Link to="/task-4">Task 4</Link></li>
+                <li><Link to="/task-5">Task 5</Link></li>
+                <li><Link to="/task-6">Task 6</Link></li>
+            </ul>
+        </nav>
+    )
 }
 
 export default Header
